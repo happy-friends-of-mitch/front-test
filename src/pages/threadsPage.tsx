0c64c5ce1743f41b0cbc5803eb3aea7e8df9a294
yusukezemi <106569487+nakami3316@users.noreply.github.com>
2023-07-15 13:36:51 +0900

    スワイプ機能の追加

--- src/pages/threadsPage.tsx
+++ src/pages/threadsPage.tsx
@@ -1,5 +1,28 @@
+import { useSwipeable } from "react-swipeable";
+
 function Thread() {
-    return <h2>Thread</h2>;
+    const image = "aa"
+    const handlers = useSwipeable({
+        onSwiped: (event) => {
+            console.log(event);
+            if (event.dir == "Up") {
+                //左にスワイプしたときに発火するイベント
+                //hogehoge()
+                console.log("Up")
+            }
+            if (event.dir == "Down") {
+                //右にスワイプしたときに発火するイベント
+                //hogehoge
+                console.log("Down")
+            }
+        },
+        trackMouse: true, //マウス操作でのスワイプを許可する場合はtrue
+    });
+    return(
+        <div {...handlers}>
+            <h2>{image}</h2>
+        </div>          
+    )
 }
 
 export default Thread;
