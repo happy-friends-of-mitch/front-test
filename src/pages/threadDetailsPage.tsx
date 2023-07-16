cbcf716c34f896e6c479220200aa1f2e886cd476
yusukezemi <106569487+nakami3316@users.noreply.github.com>
2023-07-16 16:12:23 +0900

    パスを変更

--- src/pages/threadDetailsPage.tsx
+++ src/pages/threadDetailsPage.tsx
@@ -11,7 +11,7 @@ function ThreadDetail() {
 
   return (
     <div>
-      {parsedJson.thread_name}
+      {parsedJson[0].thread_name}
       {data.map((data) => (
         <MyComponentProps img_url={data.img_url} reply_id={data.reply_id} />
       ))}
