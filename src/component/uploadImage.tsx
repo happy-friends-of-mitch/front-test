244a46c29ce0537248ce1f4d80400bda21789e67
yusukezemi <106569487+nakami3316@users.noreply.github.com>
2023-07-16 23:32:11 +0900

    update uploadmage.tsx

--- src/component/uploadImage.tsx
+++ src/component/uploadImage.tsx
@@ -1,45 +1,68 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState } from 'react'
 
 interface IdComponentProps {
-    thread_id: number
-    reply_id: number
+  thread_id: number
+  reply_id: number
 }
 
-const FileUploadComponent: React.FC<IdComponentProps> = ({ thread_id, reply_id }) => {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+const FileUploadComponent: React.FC<IdComponentProps> = ({
+  thread_id,
+  reply_id,
+}) => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
 
   function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
-        const file = event.target.files?.[0];
-        setSelectedFile(file || null);
-    }
+    const file = event.target.files?.[0]
+    setSelectedFile(file || null)
+  }
 
   const handleUpload = () => {
     if (selectedFile) {
       // アップロード処理を行うためのコードを追加する
-      console.log("Uploading file:", selectedFile.name);
+      console.log('Uploading file:', selectedFile.name)
       // ここに実際のアップロード処理を実装する
-      const reader = new FileReader();
+      const reader = new FileReader()
       reader.onload = () => {
-        const fileData = reader.result as string;
-        const base64Data = fileData.split(",")[1]; // Base64データ部分を取得
-        const fileExtension = selectedFile.name.split(".").pop(); // 拡張子を取得
+        const fileData = reader.result as string
+        const base64Data = fileData.split(',')[1] // Base64データ部分を取得
+        const fileExtension = selectedFile.name.split('.').pop() // 拡張子を取得
 
-        const jsonData = {
-          base64: base64Data,
-          extension: fileExtension,
-          thread_id: thread_id,
-          reply_id: reply_id
+        const myHeaders = new Headers()
+        myHeaders.append('Content-Type', 'application/json')
+
+        const jsonData = JSON.stringify({
+          "thread_id": thread_id,
+          "reply_id": reply_id,
+          "fileType": fileExtension,
+          "base64": base64Data,
+
+          // "reply_id": 1,
+        });
+
+        const requestOptions: RequestInit = {
+          method: 'POST',
+          headers: myHeaders,
+          body: jsonData,
+          redirect: 'follow',
         };
 
-        console.log("JSON data:", jsonData);
+        fetch(
+          "https://megatter-func.azurewebsites.net/api/TestTrigger?",
+          requestOptions
+        )
+          .then((response) => response.text())
+          .then((result) => console.log(result))
+          .catch((error) => console.log('error', error));
+
+        console.log('JSON data:', jsonData)
         // ここでJSONデータの利用や送信処理を行う
 
         // リセットする場合はコメントを外してください
         // setSelectedFile(null);
-      };
-      reader.readAsDataURL(selectedFile);
+      }
+      reader.readAsDataURL(selectedFile)
     }
-  };
+  }
 
   return (
     <div>
@@ -48,7 +71,7 @@ const FileUploadComponent: React.FC<IdComponentProps> = ({ thread_id, reply_id }
         Upload
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default FileUploadComponent;
+export default FileUploadComponent
